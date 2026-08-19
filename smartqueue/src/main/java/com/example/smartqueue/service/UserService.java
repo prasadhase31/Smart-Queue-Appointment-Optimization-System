ed3e91667279c828fa632d3f5888ebce251d507e
prasadhase31 <prasadhase9325@gmail.com>
2026-08-19 22:54:10 +0530

    Create User Service

--- smartqueue/src/main/java/com/example/smartqueue/service/UserService.java
+++ smartqueue/src/main/java/com/example/smartqueue/service/UserService.java
@@ -1,4 +1,32 @@
 package com.example.smartqueue.service;
 
+import com.example.smartqueue.dto.UserRequest;
+import com.example.smartqueue.entity.User;
+import com.example.smartqueue.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(UserRequest request) {
+
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setPhone(request.getPhone());
+        user.setRole("USER");
+        user.setCreatedAt(LocalDateTime.now());
+
+        return userRepository.save(user);
+    }
 }
