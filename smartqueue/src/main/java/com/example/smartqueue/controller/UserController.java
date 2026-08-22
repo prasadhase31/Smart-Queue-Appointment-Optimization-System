312f80e6a8f824ef7c781686c061c8659c712cb2
prasadhase31 <prasadhase9325@gmail.com>
2026-08-22 17:50:53 +0530

    via update UserController

--- smartqueue/src/main/java/com/example/smartqueue/controller/UserController.java
+++ smartqueue/src/main/java/com/example/smartqueue/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -25,5 +27,12 @@ public class UserController {
         User user = userService.createUser(request);
 
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        List<User> users = userService.getAllUsers();
+
+        return ResponseEntity.ok(users);
     }
 }
