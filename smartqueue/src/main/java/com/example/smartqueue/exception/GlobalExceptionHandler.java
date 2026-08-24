a2c6c10d46a113c8930177e7be8fd8a5499d6c3b
prasadhase31 <prasadhase9325@gmail.com>
2026-08-24 17:13:31 +0530

    create GlobalExceptionHandler

--- smartqueue/src/main/java/com/example/smartqueue/exception/GlobalExceptionHandler.java
+++ smartqueue/src/main/java/com/example/smartqueue/exception/GlobalExceptionHandler.java
@@ -1,4 +1,31 @@
 package com.example.smartqueue.exception;
 
+import com.example.smartqueue.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFound(
+            ResourceNotFoundException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(
+                response,
+                HttpStatus.NOT_FOUND
+        );
+    }
 }
