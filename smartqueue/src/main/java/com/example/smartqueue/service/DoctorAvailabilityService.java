f5ba96fa170a27019504f166ef6ad0c3b94b8853
prasadhase31 <prasadhase9325@gmail.com>
2026-09-03 21:35:30 +0530

    create DoctorAvailabilityService

---
+++ smartqueue/src/main/java/com/example/smartqueue/service/DoctorAvailabilityService.java
@@ -0,0 +1,27 @@
+package com.example.smartqueue.service;
+
+import com.example.smartqueue.entity.DoctorAvailability;
+import com.example.smartqueue.repository.DoctorAvailabilityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoctorAvailabilityService {
+
+    private final DoctorAvailabilityRepository doctorAvailabilityRepository;
+
+    public DoctorAvailabilityService(DoctorAvailabilityRepository doctorAvailabilityRepository) {
+        this.doctorAvailabilityRepository = doctorAvailabilityRepository;
+    }
+
+
+    public DoctorAvailability createAvailability(DoctorAvailability availability) {
+        return doctorAvailabilityRepository.save(availability);
+    }
+
+
+    public List<DoctorAvailability> getAllAvailability() {
+        return doctorAvailabilityRepository.findAll();
+    }
+}
