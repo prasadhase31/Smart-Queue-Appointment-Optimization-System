afb75d4ab63a78ecdf7641ae24fe00a2b2c7929e
prasadhase31 <prasadhase9325@gmail.com>
2026-09-04 14:52:59 +0530

    create AppointmentController

---
+++ smartqueue/src/main/java/com/example/smartqueue/controller/AppointmentController.java
@@ -0,0 +1,47 @@
+package com.example.smartqueue.controller;
+
+import com.example.smartqueue.entity.Appointment;
+import com.example.smartqueue.service.AppointmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointments")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    // Create Appointment
+    @PostMapping
+    public ResponseEntity<Appointment> createAppointment(
+            @RequestBody Appointment appointment) {
+
+        Appointment savedAppointment =
+                appointmentService.createAppointment(appointment);
+
+        return new ResponseEntity<>(
+                savedAppointment,
+                HttpStatus.CREATED
+        );
+    }
+
+    // Get All Appointments
+    @GetMapping
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
+
+        List<Appointment> appointments =
+                appointmentService.getAllAppointments();
+
+        return new ResponseEntity<>(
+                appointments,
+                HttpStatus.OK
+        );
+    }
+}
