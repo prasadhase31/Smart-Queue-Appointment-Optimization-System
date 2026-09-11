b0849ebe3ec19a72542b0e7a1ba78e3f44c677fc
prasadhase31 <prasadhase9325@gmail.com>
2026-09-11 15:01:59 +0530

    create AppointmentResponseDTO

--- smartqueue/src/main/java/com/example/smartqueue/dto/AppointmentResponseDTO.java
+++ smartqueue/src/main/java/com/example/smartqueue/dto/AppointmentResponseDTO.java
@@ -1,4 +1,30 @@
 package com.example.smartqueue.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AppointmentResponseDTO {
+
+    private Long id;
+
+    private UserResponseDTO patient;
+
+    private DoctorResponseDTO doctor;
+
+    private AvailabilityResponseDTO availability;
+
+    private LocalDate appointmentDate;
+
+    private LocalTime appointmentTime;
+
+    private String reason;
+
+    private String status;
 }
